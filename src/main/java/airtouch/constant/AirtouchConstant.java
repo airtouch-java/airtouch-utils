db643d80b9ec6df9feee6649c4d147a36d148b51
Net Wolf UK <netwolfuk@gmail.com>
2023-11-08 09:08:09 +1300

    Cleanup

--- src/main/java/airtouch/constant/AirtouchConstant.java
+++ src/main/java/airtouch/constant/AirtouchConstant.java
@@ -4,7 +4,7 @@ package airtouch.constant;
  * Interface to mark an Enum that can be converted from Airtouch Version specific to Generic.
  * @param <G> is the non-version specific Enum. Typically lives in <code>airtouch</code> or <code>airtouch.constants</code> package.
  */
-public interface AirtouchConstant<G extends Enum> { //  & SpecialEnum<S>
+public interface AirtouchConstant<G extends Enum<?>> {
     /**
      * Returns the non-specific Enum that is equivalent to the version specific Enum
      * @return The non-version specific Enum. Typically lives in <code>airtouch</code> or <code>airtouch.constants</code> package.
